0b2988bd6a991e4718a67922d8dc2c7b3593f5f4
cedricode <cedric.yang@ihealthlabs.com>
2017-05-23 12:57:40 +0200

    update replay

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const MongoOplog = require('mongo-oplog');
-const { MongoClient, Timestamp } = require('mongodb');
+const { MongoClient, Timestamp, ObjectID } = require('mongodb');
 const EventEmitter = require('events');
 
 const opLogEE = new EventEmitter();
@@ -9,7 +9,7 @@ const config = {
 	oplogUri: 'mongodb://localhost:27018/local',
 	oplogColl: 'oplog.rs',
 	versionAppliedColl: 'versionApplied',
-	collections: [{name: 'patients'}],
+	collections: [{name: 'measurements'}],
 	prefix: 'history_',
 }
 
@@ -28,51 +28,87 @@ function getResumePoint(db) {
 		return c.find().sort({versioning_ts: -1, _id: -1}).limit(1).toArray()
 			.then(data => {
 				if (data && data.length > 0) {
-					coll.lowerBound = data[0].oplog_ts; // lower bound to last applied
+					coll.lowerBound = data[0].versioning_ts; // lower bound to last applied
 				}
 				else {
-					coll.lowerBound = 0; // lower bound to all the previous data
+					coll.lowerBound = Timestamp(0, 0); // lower bound to all the previous data
 				}
 			});
 	}));
 }
 
 opLogEE.on('insert', data => {
-	console.log('insert op', data);
+	console.log('insert op');
 	const coll = data.ns.split('.')[1];
 	const obj = data.o || {};
 	obj.doc_id = obj._id;
 	delete obj._id;
 	obj.versioning_ts = data.ts;
-	db.collection(`${config.prefix}${coll}`).insertOne(obj).then(result => console.log(result));
+	db.collection(`${config.prefix}${coll}`).insertOne(obj);
 });
 
 opLogEE.on('update', async data => {
-	console.log('update op', data);
-	const coll = data.ns.split('.')[1];
+	console.log('update op');
+	const collName = data.ns.split('.')[1];
+	
 	const query = data.o2;
-	const doc = await db.collection(`${config.prefix}${coll}`).findOne(query);
-	const obj = data.o || {};
-	doc.oplog_ts = data.ts;
-
-	// obj might have three forms: 
-	// $set: {<field>: <value>}, 
-	// $unset: {<field>: <value>}, 
-	// <field>: <value>
-	// warning: array notation/ dot notation
-	const $set = obj.$set;
-	const $unset = obj.$unset;
-	if ($set) {
-		resolveField(doc, $set);
+	// map _id to doc_id
+	if (query._id) {
+		query.doc_id = query._id;
+		delete query._id;
 	}
-	console.log(doc);
+
+	const doc = await db.collection(`${config.prefix}${collName}`)
+		.find(query)
+		.sort({_id: -1})
+		.limit(1)
+		.toArray()
+		.then(data => data[0]);
+	doc.versioning_ts = data.ts;
+
+	const update = data.o || {};
+
+	replayUpdate(doc, update);
+	
+	await db.collection(`${config.prefix}${collName}`).insertOne(doc);
 });
 
-function resolveField(doc, key_value) {
+function replayUpdate(doc, update) {
+	const $set = update.$set;
+	const $unset = update.$unset;
+	if ($set) {
+		setField(doc, $set);
+	}
+	if ($unset) {
+		unsetField(doc, $unset);
+	}
+	delete update.$set;
+	delete update.$unset;
+	delete update._id;
+	setField(doc, update);
+	delete doc._id;
+}
+
+function unsetField(doc, key_value) {
+	for (let key in key_value) {
+		let chain = key.split('.');
+		chain.reduce((acc, cur, ind, arr) => {
+			if (!acc) {
+				return null;
+			}
+			if (ind === arr.length - 1) {
+				delete acc[cur];
+			}
+			return acc[cur];
+		}, doc)
+	}
+}
+
+function setField(doc, key_value) {
 	for (let key in key_value) {
 		let chain = key.split('.');
 		if (chain.length === 1) {
-			if (Array.isArray(doc) && typeof chain[0] === 'number') {
+			if (Array.isArray(doc) && !isNaN(parseInt(chain[0]))) {
 				doc.splice(chain[0], 0, key_value[key]);
 			} else {
 				doc[chain[0]] = key_value[key];
@@ -80,26 +116,26 @@ function resolveField(doc, key_value) {
 			continue;
 		} else {
 			if (!doc[chain[0]]) {
-				if (typeof chain[1] === 'number') {
+				if (!isNaN(parseInt(chain[1]))) {
 					doc[chain[0]] = [];
 				} else {
 					doc[chain[0]] = {};
 				}
 			}
-			resolveField(doc[chain[0]], {[chain.slice(1).join('.')]: key_value[key]});
+			setField(doc[chain[0]], {[chain.slice(1).join('.')]: key_value[key]});
 		}
 	}
 }
 
 opLogEE.on('delete', data => {
-	console.log('delete op', data)
+	//console.log('delete op', data)
 	const coll = data.ns.split('.')[1];
 	const obj = data.o || {};
 	obj.doc_id = obj._id;
 	delete obj._id;
 	obj.versioning_ts = data.ts;
 	obj.oplog_delete = true;
-	db.collection(`${config.prefix}${coll}`).insertOne(obj).then(result => console.log(result));
+	db.collection(`${config.prefix}${coll}`).insertOne(obj);
 });
 
 opLogEE.on('error', err => {
@@ -120,12 +156,13 @@ async function init(configuration) {
 	
 	// Setup resume point
 	await getResumePoint(db);
+	console.log('resume point: ', config.collections);
 }
 
 function counstructTailingQuery() {
 	const query = {$or: []};
 	config.collections.forEach(coll => {
-		query.$or.push({ns: `test.${coll.name}`, ts: {$gt: Timestamp(coll.lowerBound, 0)}})
+		query.$or.push({ns: `test.${coll.name}`, ts: {$gt: coll.lowerBound}})
 	});
 	return query;
 }
@@ -142,6 +179,7 @@ async function start() {
 		oplogReplay: true
 	}).stream();
 	st.on('data', data => {
+		console.log('new data');
 		switch(data.op) {
 			case 'i':
 				opLogEE.emit('insert', data);
