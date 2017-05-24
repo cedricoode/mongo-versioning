a2da5904ba9d1905787b6dc4776d840df6944e44
cedricode <cedric.yang@ihealthlabs.com>
2017-05-24 15:24:13 +0200

    dependency

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 const MongoVersioning = require('./index.js');
+const { MongoClient } = require('mongodb');
 
 const mongoVersioning = new MongoVersioning();
 
-mongoVersioning.init().then(() => {
-	mongoVersioning.start();
-})
+mongoVersioning.init()
+	.then(() => mongoVersioning.start())
+	// .then(() => MongoClient.connect('mongodb://localhost:27018/test'))
+	// .then(db => )
 
 process.on('unhandledRejection', (reason) => {
 	console.log(reason);
