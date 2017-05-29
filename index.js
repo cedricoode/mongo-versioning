f2a1d17827aad02038b75182c10b65dc8f60da49
cedricode <cedric.yang@ihealthlabs.com>
2017-05-29 09:39:10 +0200

    doc and stop method

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ const DEFAULT_CONFIG = {
 	prefix: 'history_',
 }
 
+/**
+ * @param {Object} config configuration for this module.
+ * @param {string} config.uri uri for original database, default: mongodb://localhost:27018/test
+ * @param {string} config.oplogUri uri for oplog database, default: mongodb://localhost:27018/local
+ * @param {string} config.oplogColl name of oplog collection, default: 'oplog.rs'
+ * @param {string} config.versionAppliedColl
+ * @param {Object[]} config.collections collections to be versioned
+ * @param {string} config.collections[].name name of collection to be versioned.
+ * @param {string} config.prefix prefix of versioning collection
+ */
 function MongoVersioning(config) {
 	this.configuration = Object.assign({}, DEFAULT_CONFIG);
 	if (config) {
@@ -38,12 +48,16 @@ function MongoVersioning(config) {
 	this.streams = {};
 }
 
+/**
+ * Start to sync versioning collection with original collection.
+ */
 MongoVersioning.prototype.start = async function start() {
+	if (this.oplogDb) return;
 	// Construct tailing query
 	const query = counstructTailingQuery(this.configuration);
 	console.log('query is: ', JSON.stringify(query));
 	this.oplogDb = await MongoClient.connect(this.configuration.oplogUri);
-	var oplog = this.oplogDb.collection('oplog.rs');
+	const oplog = this.oplogDb.collection(this.configuration.oplogColl);
 	this.mongoEE = oplog.find(query, {
 		awaitData: true, 
 		tailable: true,
@@ -81,6 +95,8 @@ MongoVersioning.prototype.start = async function start() {
 /**
  * Set up a stream to listen for collection data, this stream will dispatch 
  * relevant event data to event handlers and consume the data one by one.
+ * @private
+ * @param {string} key stream key corresponding to collection name.
  */
 MongoVersioning.prototype.setUpStream = function setUpStream(key) {
 	let count = 0;
@@ -109,7 +125,10 @@ MongoVersioning.prototype.setUpStream = function setUpStream(key) {
 	this.streams[key] = passThrough;
 }
 
-MongoVersioning.prototype.init = async function init() {
+/**
+ * Connect to versioning database.
+ */
+MongoVersioning.prototype.connect = async function connect() {
 	// Connect to targetting database
 	this.db = await MongoClient.connect(this.configuration.uri);
 	
@@ -119,10 +138,20 @@ MongoVersioning.prototype.init = async function init() {
 	return this;
 }
 
+/**
+ * Destroy oplog database connection
+ */
 MongoVersioning.prototype.stop = function stop() {
-	this.oplogEE.removeAllListeners();
+	if (this.oplogDb) {
+		this.oplogDb.close().then(() => this.oplog = null).catch(err => console.log('close db error: ', err));
+	}
+	if (this.mongoEE) {
+		this.mongoEE.removeAllListeners();
+	}
 }
-
+/**
+ * @private
+ */
 MongoVersioning.prototype.insertListener = function insertListener(data, count, strm) {
 	const collName = data.ns.split('.')[1];
 	const doc = data.o || {};
@@ -134,6 +163,9 @@ MongoVersioning.prototype.insertListener = function insertListener(data, count, 
 		.then(() => {console.log(`operation result for: ${collName}`, count); strm.resume();});
 }
 
+/**
+ * @private
+ */
 MongoVersioning.prototype.updateListener = async function updateListener(data, count, strm) {
 	const collName = data.ns.split('.')[1];
 	
@@ -160,6 +192,9 @@ MongoVersioning.prototype.updateListener = async function updateListener(data, c
 		.then(() => {console.log(`operation result for: ${collName}`, count); strm.resume();});
 }
 
+/**
+ * @private
+ */
 MongoVersioning.prototype.deleteListener = function deleteListener(data, count, strm) {
 		//console.log('delete op', data)
 	const collName = data.ns.split('.')[1];
@@ -172,11 +207,17 @@ MongoVersioning.prototype.deleteListener = function deleteListener(data, count, 
 		.then(() => {console.log(`operation result for: ${collName}`, count); strm.resume();});
 }
 
+/**
+ * @private
+ */
 MongoVersioning.prototype.opListener = function opListener(data, count, strm) {
 	console.log(`operation result for: ${data.op}`, count);
 	strm.resume();
 }
 
+/**
+ * @private
+ */
 MongoVersioning.prototype.errListener = function errListener(data, count) {
 	console.log('oplog event emitter error: ', err);	
 }
